8e1f27f88461f81a600809b35021695ae5d7e1f3
Tasjr2401 <Tasjr2401@yahoo.com>
2022-09-01 23:23:22 -0400

    commit to pull on pc

--- src/Components/BookTracker.js
+++ src/Components/BookTracker.js
@@ -2,12 +2,11 @@ import React, { useMemo, useState, useEffect } from "react";
 import { handleNumber } from "./UsefulFunctions";
 
 const BookTracker = () => {
-    const [bookInput, setBookInput] = useState({
-        Title: '',
-        Author: '',
-        PagesRead: 0,
-        Completed: false
-    })
+    const [bookTitle, setBookTitle] = useState('');
+    const [bookAuthor, setBookAuthor] = useState('');
+    const [pagesRead, setPagesRead] = useState(0);
+    const [completed, setCompleted] = useState(false);
+
     const [bookList, setBookList] = useState(() => {
         var tempArray = JSON.parse(localStorage.getItem('BookList'));
         if(!tempArray) {
@@ -27,9 +26,13 @@ const BookTracker = () => {
                 <h2>By {e.Author}</h2>
                 <h3>Pages Read: {e.PagesRead}</h3>
                 <label>Completed: </label>
-                <input value={e.Completed} type='checkbox' onChange={() => {
-                    
+                <input value={e.Completed} type='checkbox' onChange={({target}) => {
+                    var index = bookList.indexOf(e);
+                    var tempArray = [...bookList];
+                    tempArray[index].Completed = target.value;
+                    setBookList(tempArray);
                 }} />
+                <h3>Last updated: {e.LastUpdated}</h3>
             </li>
         )
     , [bookList.length]);
@@ -37,7 +40,9 @@ const BookTracker = () => {
     return (
         <div>
             <form onSubmit={AddBook}>
-
+                <input type='text' />
+                <input type='text' />
+                <input type='number' />
             </form>
         </div>
     )
